385e9fcf3915639437b91689e4e94ad1a052851d
kennedy788 <salvadorjohnkennedy78@gmail.com>
2024-09-15 08:10:36 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+let favActorFirstName = "Robert";
+
+let favActorLastName = "Downey";
+
+let fullName = favActorFirstName + " " + favActorLastName;
+
+let lowercaseName = fullName.toLowerCase();
+
+let favPetName = "FLUFFY";
+
+let capitalizedPetName = favPetName.charAt(0).toUpperCase() + favPetName.slice(1);
+
+let message = "My favorite actor is " + fullName.toUpperCase();
+
+message += ", his best show is Silicon Valley.";
+
+console.log("Favorite Actor First Name: " + favActorFirstName);
+console.log("Favorite Actor Last Name: " + favActorLastName);
+console.log("Full Name: " + fullName);
+console.log("Lowercase Name: " + lowercaseName);
+console.log("Capitalized Pet Name: " + capitalizedPetName);
+console.log("Message: " + message);
+  
